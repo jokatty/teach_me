c18d08b3a83995d9cd79559160098c5cf0241e0a
jokatty <jyotikattani@gmail.com>
2022-03-09 14:06:41 +0100

    add take picture component to open device camera

---
+++ src/components/TakePicture.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import Button from '@mui/material/Button';
+
+export default function TakePicture() {
+  return (
+    <Button variant="contained">Open Camera</Button>
+  );
+}
