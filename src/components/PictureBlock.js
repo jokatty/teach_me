a3963639ed41191be82cac9c443bbf293c8562f5
jokatty <jyotikattani@gmail.com>
2022-03-09 14:38:16 +0100

    align pictureBlock component

--- src/components/PictureBlock.js
+++ src/components/PictureBlock.js
@@ -1,11 +1,18 @@
-import { Container } from '@mui/material';
 import React from 'react';
+
+import { Container } from '@mui/material';
 import TakePicture from './TakePicture';
 import UploadPicture from './UploadPicture';
 
 export default function PictureBlock() {
+  const btnContainer = {
+    container: {
+      // width: '70%',
+      padding: '2rem',
+    },
+  };
   return (
-    <Container>
+    <Container maxWidth="sm" style={btnContainer.container}>
       <TakePicture />
       <UploadPicture />
     </Container>
