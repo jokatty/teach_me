957ef0f484644ffbbae7b98839182fc72d834ba4
jokatty <jyotikattani@gmail.com>
2022-03-09 14:04:48 +0100

    add picture block component to take or upload pictures

---
+++ src/components/PictureBlock.js
@@ -0,0 +1,11 @@
+import { Container } from '@mui/material';
+import React from 'react';
+import TakePicture from './TakePicture';
+
+export default function PictureBlock() {
+  return (
+    <Container>
+      <TakePicture />
+    </Container>
+  );
+}
