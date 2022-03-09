859fa6da89b84085fed9a521644b541a4bc8e7f1
jokatty <jyotikattani@gmail.com>
2022-03-09 14:14:07 +0100

    create component to upload pictures

---
+++ src/components/UploadPicture.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import Button from '@mui/material/Button';
+
+export default function UploadPicture() {
+  return (
+    <Button variant="contained">Upload Image</Button>
+  );
+}
