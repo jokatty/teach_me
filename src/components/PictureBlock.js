2f71a7f5b31032884ccc26d711dce65ba19b9976
jokatty <jyotikattani@gmail.com>
2022-03-09 14:13:16 +0100

    add upload picture component

--- src/components/PictureBlock.js
+++ src/components/PictureBlock.js
@@ -1,11 +1,13 @@
 import { Container } from '@mui/material';
 import React from 'react';
 import TakePicture from './TakePicture';
+import UploadPicture from './UploadPicture';
 
 export default function PictureBlock() {
   return (
     <Container>
       <TakePicture />
+      <UploadPicture />
     </Container>
   );
 }
